c6287cd6c68f3e5b3ce3fde532836a01d94790b5
Robert Sokol <robersokol.om@gmail.com>
2021-02-21 23:10:44 +0100

    add resolver function

--- dnsme.sh
+++ dnsme.sh
@@ -6,6 +6,12 @@
 ################################################################################
 
 ################################################################################
+# VARIABLES
+################################################################################
+
+_dns_record_type=(NS CNAME TXT SRV AAAA SOA)
+
+################################################################################
 # FUNCTIONS
 ################################################################################
 
@@ -13,9 +19,10 @@ dns_help() {
   cat <<- EOF
 dnsme: dnsme [DOMAIN]... [NAMESERVER]... [OPTION]...
 Show DNS information for the specified domain.
-
-  -g, --geoip   Provide GeoIP information from ip-api.com along with DNS information.
-  -h, --help    Show this help section.
+  -d, --domain    Select the domain for the query.
+  -r, --resolver  Select specific resolver for the query.
+  -g, --geoip     Provide GeoIP information from ip-api.com along with DNS information.
+  -h, --help      Show this help section.
 EOF
 }
 
@@ -23,3 +30,8 @@ dns_parse_domain() {
   _domain="$(echo "$1" | sed -e 's+https\?://++')"
   _top_domain="$(echo "$_domain" | rev | cut -d '.' -f1-2 | rev)"
 }
+
+dns_parse_resolver() {
+  _dns_resolver="$(1:-8.8.8.8)"
+}
+  
