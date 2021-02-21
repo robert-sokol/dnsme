f5aba0baa3d730ebabefce0c24533520f8d0b64c
Robert Sokol <robersokol.om@gmail.com>
2021-02-21 22:32:58 +0100

    initial commit

---
+++ dnsme.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+################################################################################
+# dnsme is a Bash script used to get various records, geoip information etc.
+# for a specific domain.
+################################################################################
+
+
